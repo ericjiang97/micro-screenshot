ee0705967118104a3d278b79ad7fd18e72efa674
Eric Jiang <eric.jiang@monash.edu>
2017-06-08 11:04:57 +1000

    Code Improvements

--- index.js
+++ index.js
@@ -8,25 +8,58 @@ var nightmare = Nightmare();
 
 const takeScreenshot = (req, res) =>{
     var targetSite = req.headers.site
-    nightmare
-        .goto(targetSite)
-        .wait(3000)
-        .viewport(1920, 1060)
-        .screenshot()
-        .then(screen => imgurUploader(screen))
-        .then(screen => {
-            send(res, 200, {"url": screen.link}) 
-            return res;
-        })
-        .catch(err => {
-            console.error(err)
-            send(err, 500, {"error": err})
-        })
+    if(targetSite){
+        nightmare
+            .goto(targetSite)
+            .wait(3000)
+            .viewport(1920, 1060)
+            .screenshot()
+            .then(screen => imgurUploader(screen))
+            .then(screen => {
+                send(res, 200, {"url": screen.link}) 
+                return res;
+            })
+            .catch(err => {
+                console.error(err)
+                send(err, 500, {"error": err})
+            })
+    } else {
+        send(res, 400, {"error": "URL not defined"})
+    }
+}
+
+const customTake = (req, res) =>{
+    var targetSite = req.headers.site;
+    var height = req.headers.height;
+    var width = req.headers.width;
+    if(targetSite){
+        if(height && width){
+            nightmare
+                .goto(targetSite)
+                .wait(3000)
+                .viewport(width, height)
+                .screenshot()
+                .then(screen => imgurUploader(screen))
+                .then(screen => {
+                    send(res, 200, {"url": screen.link}) 
+                    return res;
+                })
+                .catch(err => {
+                    console.error(err)
+                    send(err, 500, {"error": err})
+                })
+        } else {
+            send(res, 400, {"error": "Height and/or Width not defined"})
+        }
+    } else {
+        send(res, 400, {"error": "URL not defined"})
+    }
 }
 
 const notfound = (req, res) =>
   send(res, 404, 'Not found route')
 
 module.exports = router(
-  get('/take/', takeScreenshot)
+  get('/take', takeScreenshot),
+  get('/takeCustom', customTake)
 )
